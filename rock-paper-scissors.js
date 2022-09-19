65074437914b7ded7d98372bf23ab4b742b11735
Mangata <nghaddar@student.42.fr>
2022-09-19 21:19:52 +0200

    Integration worked

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,11 @@
+let round = 0;
+let result = 0;
+let playerScore = 0;
+let computerScore = 0;
+let choiceImg;
+let resetButton;
+let fightResultsUI = document.querySelector("#fight-results");
+
 function computerChoice()
 {
     let arrChoices = ["rock", "paper", "scissors"];
@@ -18,42 +26,49 @@ function playRound(player, computer)
         return ("lose");   
 }
 
-function game()
+function game(event)
 {
-    let round = 0;
-    let result = 0;
-    let playerScore = 0;
-    let computerScore = 0;
     let player_choice = "";
     let computer_choice = "";
-    let imgChoice;
 
-    imgChoice = document.querySelectorAll(img);
-    imgChoice.forEach( (el) => {el.addEventListener("click", playerChoice)})
-
-    while (round < 5) 
+    console.log(`Player choosed : ${this.alt}`);
+    player_choice = this.alt;
+    computer_choice = computerChoice();
+    result = playRound(player_choice, computer_choice);
+    switch (result)
     {
-        player_choice = playerChoice();
-        computer_choice = computerChoice();
-        result = playRound(player_choice, computer_choice);
-        switch (result)
-        {
-            case "win":
-                console.log(`Player wins : ${player_choice} beats ${computer_choice}`)
-                playerScore++;
-                break;
-            case "lose":
-                console.log(`Player loses : ${computer_choice} beats ${player_choice}`)
-                computerScore++;
-                break;
-            default:
-                console.log("Tie!")
-        }
-        console.log(`Player : ${playerScore} : CPU : ${computerScore}`);
-        round++;
+        case "win":
+            console.log(`Player wins : ${player_choice} beats ${computer_choice}`)
+            fightResultsUI.textContent = `Player wins : ${player_choice} beats ${computer_choice}`;
+            playerScore++;
+            break;
+        case "lose":
+            console.log(`Player loses : ${computer_choice} beats ${player_choice}`)
+            fightResultsUI.textContent = `Player loses : ${computer_choice} beats ${player_choice}`;
+            computerScore++;
+            break;
+        default:
+            console.log("Tie!");
+            fightResultsUI.textContent = "TIE!";
     }
 
-    if (computerScore > playerScore) { console.log("CPU WINS") };
-    if (computerScore < playerScore) { console.log("PLAYER WINS") };
-    if (computerScore == playerScore) { console.log("TIE")} ;
+    round++; 
+    console.log(`Player : ${playerScore} : CPU : ${computerScore}`);
+
+    document.querySelector(".player-score").lastElementChild.textContent =  playerScore;
+    document.querySelector(".computer-score").lastElementChild.textContent = computerScore;
+    document.querySelector(".rounds").lastElementChild.textContent = round;
+
+    if (round == 5)
+    {
+        choiceImg.forEach( (e) => e.removeEventListener("click", game));
+        console.log("event listener removed.")
+        if (computerScore > playerScore) { fightResultsUI.textContent = "CPU WINS"; }
+        if (computerScore < playerScore) { fightResultsUI.textContent = "PLAYER WINS"; }
+        if (computerScore == playerScore) { fightResultsUI.textContent = "MATCH IS TIE";}
+    }
 }
+
+
+choiceImg = document.querySelectorAll("img");
+choiceImg.forEach( (e) => e.addEventListener("click", game));
